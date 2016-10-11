60b6e47e63dde8992464987f1861307516634e77
Chris Auret <chris.auret@yahoo.co.uk>
2016-10-12 08:53:16 +1100

    Stacks and Brackets

--- LinkedLists/Program.cs
+++ LinkedLists/Program.cs
@@ -8,6 +8,23 @@ namespace LinkedLists
 {
     class Program
     {
+        public class Node
+        {
+            public int Data { get; set; }
+            public Node Next { get; set; }
+        }
+
+        public static bool HasCycle(Node head)
+        {
+            var count = 0;
+            while (head != null && count++ < 101)
+            {
+                head = head.Next;
+            }
+
+            return head != null;
+        }
+
         static void Main(string[] args)
         {
         }
---
+++ Stacks/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    class Program
+    {
+        public static bool IsBalanced(string input)
+        {
+            var pairs = new Dictionary<char, char> { { '}','{' }, { ']','[' }, { ')', '('} };
+
+            var history = new Stack<char>();    // Last in first out
+
+            foreach (var c in input)
+            {
+                if (pairs.ContainsValue(c))
+                {
+                    history.Push(c);
+                }
+
+                if (pairs.ContainsKey(c) && (history.Count== 0 || history.Pop() != pairs[c]))
+                {
+                    return false;
+                }
+            }
+
+            return history.Count == 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            //{[()]}
+            //{[(])}
+            //{{[[(())]]}}
+
+            var input = "{{[[(())]]}}";
+
+            Console.WriteLine(IsBalanced(input) ? "Yes" : "No");
+
+            Console.ReadLine();
+        }
+    }
+}
